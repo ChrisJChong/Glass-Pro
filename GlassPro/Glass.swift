919279c5b4184cd9174d897f2862e9fa21559d01
Chris <chrischong030@gmail.com>
2020-07-16 23:54:29 +1000

    Complete GlassPro

---
+++ GlassPro/Glass.swift
@@ -0,0 +1,17 @@
+//
+//  Glass.swift
+//  GlassPro
+//
+//  Created by wade chen on 8/7/20.
+//  Copyright © 2020 Chris. All rights reserved.
+//
+
+import Foundation
+
+struct Glass {
+    
+    var type = ""
+    var thickness = ""
+    var cutToSizePrice = 0.0
+    var toughenPrice = 0.0
+}
